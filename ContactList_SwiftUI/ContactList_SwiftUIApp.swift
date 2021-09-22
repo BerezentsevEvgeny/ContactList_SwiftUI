95d7512bb201a0badd2c448ac00689b7055a1202
Berezentsev Evgeny <jonn09@icloud.com>
2021-09-22 18:08:36 +0300

    Initial Commit

---
+++ ContactList_SwiftUI/ContactList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactList_SwiftUIApp.swift
+//  ContactList_SwiftUI
+//
+//  Created by Евгений Березенцев on 22.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
